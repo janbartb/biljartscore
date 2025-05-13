dc91da1ad65762593c93ae79a15ba5134097c99d
Jan Bart Biemans <j.biemans4@chello.nl>
2025-05-13 09:48:01 +0200

    added animation rest scoreborden

--- src/app/spelen/wedstrijd/wed-score/wed-score-speler/wed-score-speler.component.ts
+++ src/app/spelen/wedstrijd/wed-score/wed-score-speler/wed-score-speler.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, effect, EventEmitter, input, Input, Output } from '@angular/core';
 import { WedSpeler } from '../../../../model/wedstrijd';
 import { DecimalPipe, NgClass } from '@angular/common';
 
@@ -15,7 +15,52 @@ import { DecimalPipe, NgClass } from '@angular/common';
 export class WedScoreSpelerComponent {
     @Input() speler: WedSpeler = new WedSpeler(-1);
     @Input() grijs: boolean = false;
+    cars = input(0);
+    carsView: number = 0;
+    brtn = input(0);
+    brtnView: number = 0;
+    ser = input(0);
+    serView: number = 0;
     @Output() undoBusy: EventEmitter<boolean> = new EventEmitter<boolean>();
+    cssClassCars: string = '';
+    cssClassBrtn: string = '';
+    cssClassSer: string = '';
+
+    constructor() {
+        this.carsView = this.cars();
+        this.brtnView = this.brtn();
+        this.serView = this.ser();
+        effect(() => {
+            const c = this.cars();
+            setTimeout(() => {
+                this.cssClassCars = 'carsHidden';
+                setTimeout(() => {
+                    this.carsView = this.cars();
+                    this.cssClassCars = '';
+                }, 800);                                        
+            }, 250);
+        });
+        effect(() => {
+            const b = this.brtn();
+            setTimeout(() => {
+                this.cssClassBrtn = 'brtnHidden';
+                setTimeout(() => {
+                    this.brtnView = this.brtn();
+                    this.cssClassBrtn = '';
+                }, 800);                                        
+            }, 250);
+        });
+        effect(() => {
+            const s = this.ser();
+            setTimeout(() => {
+                this.cssClassSer = 'serHidden';
+                setTimeout(() => {
+                    this.serView = this.ser();
+                    this.cssClassSer = '';
+                }, 800);                                        
+            }, 250);
+        });
+    }
 
     balClicked() {
         this.undoBusy.emit(true);
